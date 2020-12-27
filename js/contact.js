77e7f879d320d4724b5944ca2ab6f8c9e04dcd01
tz17012016 <71942613+tz17012016@users.noreply.github.com>
2020-12-27 02:42:05 +0200

    1.7

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,19 @@
-document.getElementById("submit").addEventListener("click", myFunction);
+document.querySelector("#submit").addEventListener("click", () => {
+    let fname = document.forms["myForm"]["fname"].value;
+    let lname = document.forms["myForm"]["lname"].value;
+    let email = document.forms["myForm"]["email"].value;
+    if (fname == "") {
+        alert("Name must be filled out");
+        return false;
+    } else if (lname == "") {
+        alert("last name must be filled out");
+        return false;
+    } else if (email == "") {
+        alert("email must be filled out");
+        return false;
+    } else {
+        alert("submited");
+    }
+});
 
-function myFunction() {
-    alert("submited");
-}
+//function validateForm() {}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 // MDB Lightbox Init
 
-$(function() {
-    $("#mdb-lightbox-ui").load("../media.html");
+document.querySelector("#card-1").addEventListener("click", () => {
+    window.open(
+        "https://www.marriott.com/hotels/travel/msyis-sheraton-new-orleans-hotel/",
+        "_blank"
+    );
+});
+document.querySelector("#card-2").addEventListener("click", () => {
+    window.open("https://walk-ons.com/", "_blank");
+});
+document.querySelector("#card-3").addEventListener("click", () => {
+    window.open("https://www.acehotel.com/neworleans/", "_blank");
+});
+document.querySelector("#card-4").addEventListener("click", () => {
+    window.open("https://www.ritzcarlton.com/en/hotels/new-orleans", "_blank");
+});
+document.querySelector("#card-5").addEventListener("click", () => {
+    window.open("https://www.premiumparking.com/P402", "_blank");
+});
+document.querySelector("#card-6").addEventListener("click", () => {
+    window.open("https://www.lcm.org/", "_blank");
+});
+document.querySelector("#card-7").addEventListener("click", () => {
+    window.open("http://www.museumofdeath.net/", "_blank");
+});
+document.querySelector("#card-8").addEventListener("click", () => {
+    window.open("https://thehauntedmuseum.com/", "_blank");
 });
