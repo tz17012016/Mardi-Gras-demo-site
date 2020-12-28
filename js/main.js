558d052fb33dceb1f1d70e864a88fdd022fd33ef
tz17012016 <71942613+tz17012016@users.noreply.github.com>
2020-12-28 16:24:52 +0200

    1.9

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // MDB Lightbox Init
 
-document.querySelector("#card-1").addEventListener("click", () => {
+/*document.querySelector("#card-1").addEventListener("click", () => {
     window.open(
         "https://www.marriott.com/hotels/travel/msyis-sheraton-new-orleans-hotel/",
         "_blank"
@@ -26,4 +26,44 @@ document.querySelector("#card-7").addEventListener("click", () => {
 });
 document.querySelector("#card-8").addEventListener("click", () => {
     window.open("https://thehauntedmuseum.com/", "_blank");
+});*/
+
+///
+/*let $el, $ps, $up, totalHeight;
+$(".infoAfect .btn").on("click", function(e) {
+    e.preventDefault();
+    totalHeight = 0;
+    $el = $(this);
+    $p = $el.parent();
+    $up = $p.parent();
+    $ps = $up.find("p:not('.read-more')");
+    $ps.each(function() {
+        totalHeight += $(this).outerHeight();
+    });
+    console.log(totalHeight);
+    $up.css({
+        height: $up.outerHeight(),
+        "max-height": 9999,
+    });
+    $up.animate({
+        height: totalHeight + 200,
+    });
+    $p.fadeToggle(2000);
+    return false;
+});*/
+
+$(".read-more").click(function(e) {
+    let hrefAr = [
+        "https://www.marriott.com/hotels/travel/msyis-sheraton-new-orleans-hotel/",
+        "https://walk-ons.com/",
+    ];
+    e.preventDefault();
+    hrefAr.map((item) => {
+        $(this)
+            .text(function(i, t) {
+                return t == "close" ? "read-more" : "close";
+            })
+            .prev(".infoAfect")
+            .slideToggle();
+    });
 });
